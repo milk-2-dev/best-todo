f19ade7f0ac199f6d497724bcb7c778168c3b571
milk <milk00000000000000@gmail.com>
2026-02-12 17:14:24 +0100

    migrate to react-router

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,8 @@
-import path from "path"
-
-import { vitePlugin as remix } from "@remix-run/dev";
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import react from '@vitejs/plugin-react'
-
 
 export default defineConfig({
-  plugins: [
-    remix(),
-    tsconfigPaths(),
-    react(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./app"),
-    },
-  },
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
